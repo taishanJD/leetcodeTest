1ba71a0b9fd85fa8986924fa22ae4d8038490d14
taishanJD <15603395914@163.com>
2016-05-04 21:09:15 +0800

    160504用两个栈实现队列

---
+++ src/code160504/Solution.java
@@ -0,0 +1,27 @@
+package code160504;
+
+import java.util.Stack;
+
+/*
+ * 2016.05.04
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * */
+public class Solution {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int value) {
+	while (!stack2.isEmpty()) {
+	    stack1.push(stack2.pop());
+	}
+	stack1.push(value);
+    }
+
+    public int pop() {
+	while (!stack1.isEmpty()) {
+	    stack2.push(stack1.pop());
+	}
+	return stack2.pop();
+    }
+}
